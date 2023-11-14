e9e95a32efe0ed485a585d717b812d9ffa1b5b0d
Lucas Fan <xfan0126@gmail.com>
2023-11-13 22:13:33 -0800

    github pages

--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,8 @@ const router = createBrowserRouter(
             </Route>
             <Route path="/sign-in" element={<SignInPage />} />
         </>
-    )
+    ),
+    { basename: "ce-tracker" }
 );
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
