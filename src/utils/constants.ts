f8a026c45ce2b5ee176714a6a4260260fe04bbf5
Lucas Fan <xfan0126@gmail.com>
2024-02-23 21:45:54 -0800

    changed backend server address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const BACKEND_SERVER_ADDRESS =
-    "https://f641-97-93-141-229.ngrok-free.app";
+    "https://e693-97-93-141-229.ngrok-free.app";
 export const JWT_LOCAL_STORAGE_KEY = "jwt";
